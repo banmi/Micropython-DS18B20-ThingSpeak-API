62863f460fa4e31e83d4c905bd780f90a77d7292
banmi <regosmichal@gmail.com>
2021-12-02 17:21:21 +0100

    Clean code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import ds18x20
 import urequests
 import time
 import credentials
+from credentials import thing_api
 
 
 # **************************************
@@ -17,17 +18,18 @@ ds_pin =onewire.OneWire(Pin(4))
 ds_sensor = ds18x20.DS18X20(ds_pin)
 roms = ds_sensor.scan()
 
-#ssid = 'netis'
-#password = 'password'
 # **************************************
 # Configure the ESP32 wifi as STAtion
 sta = network.WLAN(network.STA_IF)
+
+# Configure static IP
 sta.ifconfig(['192.168.8.108', '255.255.255.0', '192.168.8.1', '192.168.8.1'])
+
 if not sta.isconnected():
     print('connecting to network...')
     sta.active(True)
 
-    # sta.connect('your wifi ssid', 'your wifi password')
+# sta.connect('your wifi ssid', 'your wifi password')
     sta.connect(ssid, password)
     while not sta.isconnected():
         pass
@@ -37,17 +39,17 @@ print('network config:', sta.ifconfig())
 # **************************************
 # Constants and variables:
 HTTP_HEADERS = {'Content-Type': 'application/json'}
-#THINGSPEAK_WRITE_API_KEY = apikey
+
 # initially there would be some delays
 # before submitting the first update
 # but should be enough to stabilize the
-# the DHT sensor.
-#global temp
+# the DS/DHT sensor.
+
 
 def request(ds_readings):
     request = urequests.post(
       'http://api.thingspeak.com/update?api_key=' +
-      apikey,
+      thing_api,
       json=ds_readings,
       headers=HTTP_HEADERS)
     request.close()
@@ -55,19 +57,18 @@ def request(ds_readings):
 # **************************************
 # Main loop:
 while True:
-    led.off() # led off will turn led on
-    roms = ds_sensor.scan()
+    led.off() # led off will turn led on (Board bug)
+    
     ds_sensor.convert_temp()
     time.sleep_ms(3000)
     
     for rom in roms:
-        
-      print("read")
-      time.sleep_ms(2000)
+     print("read")
+     time.sleep_ms(2000)
     temp = ds_sensor.read_temp(rom)
     ds_readings = {'field1': temp}
 
-    if temp > 30:
+    if temp >= 30:
         request(ds_readings)  
         print("sending to server")
     print(temp)
